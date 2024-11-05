34dbdd94716fb4f4cbc945b9e1717dc183b1ed57
marcoLapiello <marcolapiello@gmail.com>
2024-11-05 11:41:02 +0100

    The two values for the class "Contact" get defined in the addContact()

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ class Contact {
 }
 
 function addContact(firstName, lastName){
-    
     let myContact = new Contact();
-    
+    myContact['firstName'] = document.getElementById("firstNameInput").value
+    myContact['lastName'] = document.getElementById("lastNameInput").value
     contacts.push(myContact);
     console.log(contacts);
     
