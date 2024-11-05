1ff93b7709a16f1a8dce111504bd63f1739d9f06
marcoLapiello <marcolapiello@gmail.com>
2024-11-05 12:14:34 +0100

    Constructor() inserted to the class "Contact" and contacts rendering  added

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 let contacts = [];
 
 class Contact {
-    firstName;
-    lastName
+  firstName;
+  lastName;
+  constructor(name, surname) {
+    this.firstName = name;
+    this.lastName = surname;
+  }
 }
 
-function addContact(firstName, lastName){
-    let myContact = new Contact();
-    myContact['firstName'] = document.getElementById("firstNameInput").value
-    myContact['lastName'] = document.getElementById("lastNameInput").value
-    contacts.push(myContact);
-    console.log(contacts);
-    
+function addContact() {
+  let name = document.getElementById("firstNameInput").value;
+  let surname = document.getElementById("lastNameInput").value;
+  let myContact = new Contact(name, surname);
+  contacts.push(myContact);
+  console.log(contacts);
+  renderContacts();
+}
+
+function renderContacts (){
+    let contact = "";
+    let contentRef = document.getElementById("contactsContainer");
+    contentRef.innerHTML = "";
+    for (let index = 0; index < contacts.length; index++) {
+        contact = contacts[index];
+        contentRef.innerHTML += getContactTemplate(contact, index);
+    }
+}
+
+function getContactTemplate(contact, index) {
+    return /*html*/`
+        <div id="contactNr${index}">
+            <p>${contact.firstName}</p>
+            <p>${contact.lastName}</p>
+        </div>
+    `
 }
